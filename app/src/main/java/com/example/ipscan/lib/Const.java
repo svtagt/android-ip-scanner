cdf751e516fca7e99a932d0b1ea6e77228bfa2be
Svyatoslav Evdochenko <svtagt@gmail.com>
2019-03-21 11:57:50 +0300

    remame network service(!), make service utils and put create notification logic to it, minor improvements

--- app/src/main/java/com/example/ipscan/lib/Const.java
+++ app/src/main/java/com/example/ipscan/lib/Const.java
@@ -3,8 +3,13 @@ package com.example.ipscan.lib;
 public class Const {
   public static final String LOG_TAG = "IPSCAN_LIB";
 
+  public static final String EXTRA_SERVICE_TITLE = "EXTRA_SERVICE_TITLE";
+  public static final String EXTRA_SERVICE_TEXT = "EXTRA_SERVICE_TEXT";
+  public static final String EXTRA_SERVICE_TICKER = "EXTRA_SERVICE_TICKER";
+
   public static final String EXTRA_SCAN_PARAMS = "EXTRA_SCAN_PARAMS";
   public static final String EXTRA_TASK_ID = "EXTRA_TASK_ID";
+
   public static final String REPORTS_DIR_NAME = "IPScanReports";
   public static final String ACTION_START_NETWORK_SERVICE = "ACTION_START_NETWORK_SERVICE";
   public static final String ACTION_SET_NETWORK_SERVICE_ALARM = "ACTION_SET_NETWORK_SERVICE_ALARM";
@@ -15,7 +20,8 @@ public class Const {
 
   //TODO: don't use fixed value
   public static final int NUM_THREADS_FOR_PORT_SCAN = 500;
-  public static final int ONGOING_NOTIFICATION_ID = 1;
+  public static final int GET_SCAN_JOB_SERVICE_NOTIFICATION_ID = 1;
+  public static final int SCAN_SERVICE_NOTIFICATION_ID = 2;
   public static final String CHANNEL_ID = "IPSCAN_CHANNEL_ID";
 
   public static final String API_PREFIX = "https://r.skaro.icu/scanner";
--- app/src/main/java/com/example/ipscan/lib/services/GetScanJobService.java
+++ app/src/main/java/com/example/ipscan/lib/services/GetScanJobService.java
@@ -1,31 +1,27 @@
 package com.example.ipscan.lib.services;
 
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.example.ipscan.MainActivity;
 import com.example.ipscan.R;
 import com.example.ipscan.lib.Const;
 import com.example.ipscan.lib.api.FetchDataListener;
 import com.example.ipscan.lib.applied.DeviceInfo;
+import com.example.ipscan.lib.applied.ServiceUtils;
 import com.example.ipscan.lib.requests.MainRequests;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Objects;
 
-public class NetworkService extends Service {
-  static final int NOTIFICATION_ID = 543;
+
+public class GetScanJobService extends Service {
   public static boolean isServiceRunning = false;
 
   private AlarmManager alarmMgr;
@@ -35,9 +31,9 @@ public class NetworkService extends Service {
   @Override
   public void onCreate() {
     super.onCreate();
-    goToForegroundMode();
+
     alarmMgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-    Intent intent = new Intent(NetworkService.this, NetworkService.class);
+    Intent intent = new Intent(GetScanJobService.this, GetScanJobService.class);
     intent.setAction(Const.ACTION_SET_NETWORK_SERVICE_ALARM);
     alarmIntent = PendingIntent.getService(this, 0, intent, 0);
 
@@ -51,12 +47,12 @@ public class NetworkService extends Service {
 
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
-    Log.d(Const.LOG_TAG, "NetworkService onStartCommand");
+    Log.d(Const.LOG_TAG, "GetScanJobService onStartCommand");
     if (intent != null) {
-      if (intent.getAction().equals(Const.ACTION_START_NETWORK_SERVICE)) {
+      if (Objects.equals(intent.getAction(), Const.ACTION_START_NETWORK_SERVICE)) {
         goToForegroundMode();
       } else {
-        if (intent.getAction().equals(Const.ACTION_SET_NETWORK_SERVICE_ALARM)) {
+        if (Objects.equals(intent.getAction(), Const.ACTION_SET_NETWORK_SERVICE_ALARM)) {
           handleAlarm();
         } else {
           stopService();
@@ -69,7 +65,6 @@ public class NetworkService extends Service {
     return START_STICKY;
   }
 
-  // In case the service is deleted or crashes some how
   @Override
   public void onDestroy() {
     isServiceRunning = false;
@@ -78,7 +73,6 @@ public class NetworkService extends Service {
 
   @Override
   public IBinder onBind(Intent intent) {
-    // Used only in case of bound services.
     return null;
   }
 
@@ -88,41 +82,8 @@ public class NetworkService extends Service {
       return;
     }
     isServiceRunning = true;
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      CharSequence name = getString(R.string.channel_name);
-      String description = getString(R.string.channel_description);
-      int importance = NotificationManager.IMPORTANCE_DEFAULT;
-      NotificationChannel channel = new NotificationChannel(Const.CHANNEL_ID, name, importance);
-      channel.setDescription(description);
-      // Register the channel with the system; you can't change the importance
-      // or other notification behaviors after this
-      NotificationManager notificationManager = getSystemService(NotificationManager.class);
-      notificationManager.createNotificationChannel(channel);
-    }
-
-    Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-    notificationIntent.setAction(Intent.ACTION_MAIN);  // A string containing the action name
-    notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-    PendingIntent contentPendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-//    Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.mipmap.ic_launcher);
-    Notification notification = new NotificationCompat.Builder(this)
-      .setContentTitle(getResources().getString(R.string.app_name))
-      .setTicker(getResources().getString(R.string.app_name))
-      .setContentText(getResources().getString(R.string.add_scan_job))
-      .setSmallIcon(R.mipmap.ic_launcher)
-//      .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
-      .setContentIntent(contentPendingIntent)
-      .setOngoing(true)
-      .setChannelId(Const.CHANNEL_ID)
-      .build();
-
-    //TODO check it
-    // NO_CLEAR makes the notification stay when the user performs a "delete all" command
-    notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
-
-    startForeground(NOTIFICATION_ID, notification);
+    startForeground(Const.GET_SCAN_JOB_SERVICE_NOTIFICATION_ID, ServiceUtils.createNotification(
+      this, R.string.get_scan_job_service_title, R.string.get_scan_job_service_descr));
   }
 
   void stopService() {
@@ -138,7 +99,6 @@ public class NetworkService extends Service {
       public void onFetchSuccess(int status, JSONObject res) throws JSONException {
         Log.d(Const.LOG_TAG, "SUCCESS! status: " + status + " RES: " + res.toString());
         startScanService(res.getString("taskId"), res.getString("cmd"));
-//        startScanService(res.getString("taskId"), "-h 62.109.9.97-62.109.9.98 -p 1-1024");
       }
 
       @Override
@@ -152,7 +112,6 @@ public class NetworkService extends Service {
       }
     });
   }
-
 
   private void startScanService(String taskId, String cmd) {
     Intent intent = new Intent(this, ScanService.class);
